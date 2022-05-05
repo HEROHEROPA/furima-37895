52bace66a61fdca1c8d53f1210d1b5964857deed
HEROHEROPA <61680482+HEROHEROPA@users.noreply.github.com>
2022-05-06 08:25:54 +0900

    ユーザー管理機能単体テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,9 @@ RSpec.describe User, type: :model do
 
 
   describe 'ユーザー新規登録' do
+    it '全ての項目に適切な値が入力されると新規登録ができる' do
+      @user.valid?
+    end
     it 'メールアドレスが空では登録できない' do
       @user.email=""
       @user.valid?
@@ -16,12 +19,12 @@ RSpec.describe User, type: :model do
     end  
    
     it '登録済みのメールアドレスでは登録できない' do
-      @user.password = "000c000"
+      # @user.password = "000c000"
       @user.password_confirmation=@user.password
       @user.save
       another_user=FactoryBot.build(:user)
       another_user.email=@user.email
-      another_user.password = '000d000'
+      # another_user.password = '000d000'
       another_user.password_confirmation=another_user.password
       another_user.valid?
       expect(another_user.errors.full_messages).to include("Email has already been taken")
@@ -60,12 +63,71 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password is invalid")
     end  
 
+    it 'passwordに全角文字が入っていると登録できない' do    
+      @user.password = 'kaeid T G9'
+      @user.password_confirmation=@user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid")
+    end
     it 'passwordが確認用passwordと不一致では登録できない' do
-      @user.password=Faker::Internet.password(min_length: 6)
+      @user.password=Faker::Internet.password(min_length: 6, mix_case: true)
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end  
 
+    it '苗字が未記入では登録できない' do
+      @user.last_name = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end  
+
+    it '名前が未記入数字では登録できない' do
+      @user.first_name = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end  
+
+    it '苗字に半角文字が入っていると登録出来ない' do
+      @user.last_name = "k花田"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name is invalid. Input full-width characters.")
+    end  
+
+    it '名前に半角文字が入っていると登録できない' do
+      @user.first_name = "k一郎"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name is invalid. Input full-width characters.")
+    end  
+
+    it '苗字(カナ)が未記入では登録できない' do
+      @user.last_name_k = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name k can't be blank")
+    end 
+    
+    it '名前(カナ)が未記入では登録できない' do
+      @user.first_name_k = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name k can't be blank")
+    end 
+    
+    it '苗字(カナ)に半角文字が入っていると登録できない' do
+      @user.last_name_k="kハナダ"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name k is invalid. Input full-width katakana characters.")
+    end 
+
+    it '名前(カナ)に半角文字が入っていると登録できない' do
+      @user.first_name_k="kイチロウ"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name k is invalid. Input full-width katakana characters.")
+    end 
+   
+    it '生年月日が未記入だと登録できない' do
+      @user.birth_date=""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birth date can't be blank")
+    end 
 
   end
 
@@ -75,10 +137,3 @@ end
 
 
 
-# メールアドレスが必須であること。
-#  メールアドレスが一意性であること。
-#  メールアドレスは、@を含む必要があること。
-#  パスワードが必須であること。
-#  パスワードは、6文字以上での入力が必須であること
-#  パスワードは、半角英数字混合での入力が必須であること
-#  パスワードとパスワード（確認）は、値の一致が必須であること。
