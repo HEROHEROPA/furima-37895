fc9c817eeac419f4128952c75f5aa178b9a3c15e
HEROHEROPA <61680482+HEROHEROPA@users.noreply.github.com>
2022-05-12 12:22:38 +0900

    商品削除機能実装修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
 before_action :get_item  , only: [:show,:update,:edit,:destroy]
-before_action :authenticate_user!, except: [:index, :show,:destroy] 
+before_action :authenticate_user!, except: [:index, :show] 
 
   def index
     @items = Item.all.includes(:user).order('created_at DESC')
